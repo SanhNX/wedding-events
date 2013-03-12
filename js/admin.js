67760632f5001db7a2228dadde3819012223ca62
nittin990@gmail.com <nittin990@gmail.com@aae4b55f-12d5-5f43-fc5c-22d706c07eec>
2013-03-12 18:49:29 +0000

    create nav management

---
+++ js/admin.js
@@ -0,0 +1,121 @@
+﻿
+var POPUP_SOURCE = "popup-source";
+$(function () {
+    $.ajax({
+        type: "GET",
+        url: "xml/nav.xml",
+        dataType: "xml",
+        success: function (xml) {
+            $(xml).find('item').each(function () {
+                var text = $(this).attr('text');
+                var href = $(this).attr('href');
+                var item = document.createElement('li');
+                item.className = 'anav-item';
+                var link = document.createElement('input')
+                link.type = 'text';
+                link.className = 'anav-link';
+                link.value = text;
+                var url = document.createElement('input')
+                url.type = 'text';
+                url.className = 'anav-url';
+                url.value = href;
+
+                var list = document.createElement('ul')
+                list.className = 'anav-sub-list';
+                $(this).find('subItem').each(function () {
+                    var text = $(this).attr('text');
+                    var href = $(this).attr('href');
+                    var subItem = document.createElement('li');
+                    subItem.className = 'anav-sub-item';
+                    var subLink = document.createElement('input')
+                    subLink.type = 'text';
+                    subLink.className = 'anav-sub-link';
+                    subLink.value = text;
+                    var subUrl = document.createElement('input')
+                    subUrl.type = 'text';
+                    subUrl.className = 'anav-sub-url';
+                    subUrl.value = href;
+
+                    var subList = document.createElement('ul')
+                    subList.className = 'anav-sub2-list';
+                    $(this).find('subItem2').each(function () {
+                        var text = $(this).attr('text');
+                        var href = $(this).attr('href');
+                        var subItem2 = document.createElement('li');
+                        subItem2.className = 'anav-sub2-item';
+                        var subLink2 = document.createElement('input')
+                        subLink2.type = 'text';
+                        subLink2.className = 'anav-sub2-link';
+                        subLink2.value = text;
+                        var subUrl2 = document.createElement('input')
+                        subUrl2.type = 'text';
+                        subUrl2.className = 'anav-sub2-url';
+                        subUrl2.value = href;
+                        subItem2.appendChild(subLink2);
+                        subItem2.appendChild(subUrl2);
+                        subList.appendChild(subItem2);
+                    });
+
+                    subItem.appendChild(subLink);
+                    subItem.appendChild(subUrl);
+                    subItem.appendChild(createElement('a', 'btn-add'));
+                    subItem.appendChild(createElement('a', 'btn-remove'));
+                    subItem.appendChild(subList);
+                    list.appendChild(subItem);
+                });
+
+                item.appendChild(link);
+                item.appendChild(url);
+                item.appendChild(createElement('a', 'btn-add'));
+                item.appendChild(createElement('a', 'btn-remove'));
+                item.appendChild(list);
+                document.getElementById('admin-panel').appendChild(item);
+            });
+
+            loadModifyItem();
+
+        }
+    });
+});
+
+function loadModifyItem() {
+    $('.btn-remove').click(function () {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+
+    });
+    $('.btn-add').click(function () {
+        var nameSpace = $(this.parentNode).children('ul')[0].className.replace('-list', '');
+
+        var newItem = createElement('li', nameSpace + '-item');
+        newItem.className = 'anav-item';
+        var newLink = document.createElement('input', nameSpace + '-link')
+        newLink.type = 'text';
+        var newUrl = document.createElement('input', nameSpace + '-url')
+        newUrl.type = 'text';
+
+        var newList = document.createElement('ul')
+        newList.className = nameSpace + '-list';
+        if (nameSpace == 'anav-sub') newList.className = nameSpace + '2-list';
+
+        newItem.appendChild(newLink);
+        newItem.appendChild(newUrl);
+        if (nameSpace != 'anav-sub2')
+            newItem.appendChild(createElement('a', 'btn-add'));
+        newItem.appendChild(createElement('a', 'btn-remove'));
+        newItem.appendChild(newList);
+        $(this.parentNode).children('ul')[0].appendChild(newItem);
+        $('.btn-add').unbind("click");
+        loadModifyItem();
+    });
+};
+function createElement(tagName, id, className) {
+    var elem = document.createElement(tagName);
+    elem.id = id;
+    elem.className = className;
+    return elem;
+};
+function createElement(tagName, className) {
+    var elem = document.createElement(tagName);
+    elem.className = className;
+    return elem;
+};
--- js/temp.js
+++ js/temp.js
@@ -6,7 +6,7 @@ $(function () {
         });
     });
     $('.product-item').click(function () {
-        $('#popup').css({ display: "block", opacity: 0 }).animate({ opacity: 1 },250);
+        $('#popup').css({ display: "block", opacity: 0 }).animate({ opacity: 1 }, 250);
         bindingPopup(this.getAttribute("data-index"));
     });
     $('#popup-button-next').click(function () {
@@ -16,15 +16,79 @@ $(function () {
         bindingPopup(index);
     });
     $('#popup-button-prev').click(function () {
-        var index = document.getElementById(POPUP_SOURCE).getAttribute("data-index");
+        var index = document.getElementById(POPUP_SOURCE).getAttribute('data-index');
         index--;
         if (index < 0) index = $('.product-item').length - 1;
         bindingPopup(index);
     });
+    loadFB(document);
+
+    document.getElementById("main-nav").innerHTML = "";
+    $.ajax({
+        type: "GET",
+        url: "xml/nav.xml",
+        dataType: "xml",
+        success: function (xml) {
+            $(xml).find('item').each(function () {
+                var text = $(this).attr('text');
+                var href = $(this).attr('href');
+                var item = document.createElement('li');
+                item.className = 'nav-item';
+                var link = document.createElement('a')
+                link.className = 'nav-link';
+                link.innerText = text;
+
+                var list = document.createElement('ul')
+                list.className = 'nav-sub-list';
+                $(this).find('subItem').each(function () {
+                    var text = $(this).attr('text');
+                    var href = $(this).attr('href');
+                    var subItem = document.createElement('li');
+                    subItem.className = 'nav-sub-item';
+                    var subLink = document.createElement('a')
+                    subLink.className = 'nav-sub-link';
+                    subLink.innerText = text;
+
+                    var subList = document.createElement('ul')
+                    subList.className = 'nav-sub2-list';
+                    $(this).find('subItem2').each(function () {
+                        var text = $(this).attr('text');
+                        var href = $(this).attr('href');
+                        var subItem2 = document.createElement('li');
+                        subItem2.className = 'nav-sub2-item';
+                        var subLink2 = document.createElement('a')
+                        subLink2.className = 'nav-sub2-link';
+                        subLink2.innerText = text;
+                        subItem2.appendChild(subLink2);
+                        subList.appendChild(subItem2);
+                    });
+
+                    subItem.appendChild(subLink);
+                    subItem.appendChild(subList);
+                    list.appendChild(subItem);
+                });
+
+                item.appendChild(link);
+                item.appendChild(list);
+                document.getElementById('main-nav').appendChild(item);
+            });
+        }
+    });
+
 
 });
+// Load the SDK Asynchronously
+function loadFB(d) {
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
+    ref.parentNode.insertBefore(js, ref);
+};
 function bindingPopup(index) {
     document.getElementById(POPUP_SOURCE).innerHTML =
                 $('.product-item[data-index=' + index + ']').children(".item-source").html();
     document.getElementById(POPUP_SOURCE).setAttribute("data-index", index);
+    document.getElementById("popup-fb-comments").setAttribute("data-href", "http://www.haivl.com/photo/19537" + index);
+    FB.XFBML.parse(document.getElementById('popup-form-right'));
 }
